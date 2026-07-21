511f87b35f745200b292a913e54fc489c2182621
DevHarsh <harshituprety17@outlook.com>
2026-07-21 22:39:05 +0530

    signup function

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from app.models import Registration
+from app.forms import RegistrationForm
 # Create your views here.
 def home(req):
     return render(req,'home.html')
@@ -8,6 +9,37 @@ def about(req):
 def contact(req):
     return render(req,'contact.html')
 def signup(req):
-    return render(req,'signup.html')
+    form=RegistrationForm()
+    if req.method=="POST":      
+        form=RegistrationForm(req.POST)
+    
+        if form.is_valid():
+            stu_name=form.cleaned_data["stu_name"]
+            stu_email=form.cleaned_data["stu_email"]
+            stu_city=form.cleaned_data["stu_city"]
+            stu_mobile=form.cleaned_data["stu_mobile"]
+            stu_password=form.cleaned_data["stu_password"]
+            stu_confirm_password=form.cleaned_data["stu_confirm_password"]
+
+            print(stu_name,stu_email,stu_city,stu_mobile)
+            
+            if stu_password != stu_confirm_password:
+                msg = "Password Not Matching"
+                return render(req,"singup.html",{"form":form,"msg":msg})
+
+            user = StudentModel.objects.filter(stu_email=stu_email)
+            
+            if user:
+                msg = "Email already exit"
+                form = RegistrationForm()
+                return render(req,"signup.html",{"form":form,"msg":msg})
+            else:
+                form.save()
+                msg="Registration succesfull"
+                form=RegistrationForm()
+                return render(req,"signup.html",{"form":form,"msg":msg})
+            
+    else:
+        return render(req,'signup.html',{"form":form})
 def login(req):
     return render(req,'login.html')
